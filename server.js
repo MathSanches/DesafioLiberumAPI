ab78a6dff519f99d673125625001cb598239a7f5
Mmariano <m.marianosanches@gmail.com>
2025-03-05 02:27:37 -0300

    update

--- server.js
+++ server.js
@@ -107,6 +107,10 @@ app.delete('/clientes/:id', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000...");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}...`);
 });
+
+export default app;
